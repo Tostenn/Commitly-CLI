d90ea7daa1f9dea56db6e1a59dc4e9540317a39f
Tostenn <kouyatosten@gmail.com>
2025-04-09 20:07:09 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+
+"""
+creer un outil qui genere des message de commit adapter 
+a la structure de ton projet
+
+les commandes git qui me seront utile :
+    git add <file> | pour ajouter un fichier au commit
+    git commit -m "<message>" | pour faire un commit avec un message
+    git push | pour envoyer les commits sur le serveur distant
+    
+
+commandes :
+    main.py --add <file>
+        --delete <file> | pour désintexer un fichier
+        --commit-style <path_file (txt)> | pour ajouter de recommendations a l'ia
+        --push | pour envoyer les commits sur le serveur distant
+        --help | pour afficher l'aide
+        
+"""
