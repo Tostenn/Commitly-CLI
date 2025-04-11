a996478ba474b3017f52cbbfb8dfe6d7eea5ab46
Tostenn <kouyatosten@gmail.com>
2025-04-11 12:24:03 +0200

    feat: Ajout de la classe Commitly pour la génération automatique de messages de commit

---
+++ commitly.py
@@ -0,0 +1,57 @@
+"""
+
+"""
+
+
+from g4f.client import Client
+from g4f.models import gpt_3_5_turbo
+from prompt import PROMPT, STYLE_COMMIT, FORMAT_COMMIT, RECOMMANDATION
+from subprocess import run
+
+class Commitly:
+    
+    """
+    automatically generate a commit message based on the provided diff
+    """
+    
+    def __init__(self):
+        self.client = Client()
+        self.model = gpt_3_5_turbo
+        pass
+    
+    def get_value(self, style_commit:str=None, format_commit:str=None, recommandation_commit:str=None):
+        if not style_commit:
+            style_commit = STYLE_COMMIT
+        if not format_commit:
+            format_commit = FORMAT_COMMIT
+        if not recommandation_commit:
+            recommandation_commit = RECOMMANDATION
+        
+        return PROMPT.format(STYLE_COMMIT=style_commit, FORMAT_COMMIT=format_commit, RECOMMANDATION=recommandation_commit)
+        pass
+    
+    def msg_commit(self, style_commit:str=None, format_commit:str=None, recommandation_commit:str=None)-> str:
+        
+        cmd = self.cmds("git diff --cached")
+        
+        response = self.client.chat.completions.create(
+        # model="gpt-4o-mini",
+        model=self.model,
+        messages=[
+            {"role": "system", "content": self.get_value(style_commit, format_commit, recommandation_commit) },
+            
+            {"role": "user", "content": cmd}
+        ],
+        web_search=False
+    )
+        return response.choices[0].message.content
+          
+    def commit(self, message:str)-> bool:
+        cmd = self.cmds(f"""git commit -m "{message}" """, True)
+        return cmd == 0
+    
+    def cmds(cmd,code=False):
+        '''exercuter des commande shell'''
+        cmd = run(cmd,capture_output=True,text=True,shell=True)
+        if code: return cmd.returncode
+        return cmd.stdout.strip()
