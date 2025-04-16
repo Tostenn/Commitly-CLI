db1ff067b54a97d022ee331b3670929a36962faf
Tostenn <kouyatosten@gmail.com>
2025-04-16 21:08:38 +0200

    feat[commitly]: Ajout de la fonctionnalité de proposition de factorisation de commits
    - Ajout d'une option pour proposer de factoriser un commit en plusieurs plus petits.
    - Modification de la gestion des messages de commit pour inclure la liste des fichiers concernés.
    - Amélioration de l'affichage des messages et des fichiers dans la console.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from commitly.commitly import Commitly, FORMAT_COMMIT, STYLE_COMMIT, RECOMMANDAT
 from rich.prompt import Prompt
 from rich.console import Console
 from rich.panel import Panel
+from rich.text import Text
+from rich.columns import Columns
 from logo import LOGO, VERSION
 
 # Initialize argument parser
@@ -22,6 +24,7 @@ parser.add_argument("--show-style", action="store_true", help="Show the style co
 parser.add_argument("--show-recommandation", action="store_true", help="Show the recommandation commit")
 
 parser.add_argument("--comfirm", action="store_true", help="Comfirm the message of the commit")
+parser.add_argument("--fact", action="store_true", help="propose de factoriser le commit en plusieurs plus petits")
 parser.add_argument("--path-file-temp", action="store_true", help="Show the path of the temporary file", default="commit.txt")
 parser.add_argument("--del-temp", action="store_true", help="Delete the temporary file")
 parser.add_argument("-c","--continue",dest="continues", action="store_true", help="Continue the commit")
@@ -98,57 +101,76 @@ if options.add:
                 style_commit=style_commit, 
                 format_commit=format_commit,
                 recommandation_commit=recommandation_commit,
-                ticket=options.ticket
+                ticket=options.ticket,
+                fact=options.fact
             )
         else :
             try:
                 with open(commitly.file_temp, "r", encoding="utf-8") as f:
                     msg = f.read()
+                
+                msg = {
+                    'commit': msg,
+                    'files':commitly.file_stage()
+                }
             except: 
                 console.print(f"file {commitly.file_temp} not found.")
                 exit()
         if msg:
+            if isinstance(msg, dict):
+                msg = [msg]
             
             console.print("[green]✔️  Commit message generated.[/green]")
-            console.print(
-                Panel.fit(
-                    console.render_str(msg),
-                    title="Commit message"
+            for _ in msg:
+                m = _['commit']
+                files = _['files']
+                console.print(
+                    Panel.fit(
+                        console.render_str(m),
+                        title="Commit message"
+                    )
                 )
-            )
+                table = [ Panel(Text(file, justify="left"), border_style="bold green", ) for file in files[:3]]
+                if len(files) > 3:
+                    table.append(Panel(f"+ {len(files) - 3} more files", border_style="bold blue"))
+                    
+                console.print(Columns(table, equal=False))
             
-            save = commitly.save_message_to_file(msg)
-            if not save:
-                console.print("[bold red]❌  Error saving commit message. [/bold red]")
-                exit()
+                save = commitly.save_message_to_file(m)
+                if not save:
+                    console.print("[bold red]❌  Error saving commit message. [/bold red]")
+                    continue
+                    
+                if options.comfirm:
+                    c = Prompt.ask(
+                        prompt="Comfirm the message of the commit ? ",
+                        default="y", 
+                        show_default=True,
+                        show_choices=True,
+                        choices=["y", "n"],
+                    )
+                    
+                    if c.lower() != "y":
+                        if options.del_temp:
+                            commitly.file_temp.unlink()
+                        
+                        if add != '!': commitly.unstage(add)
+                        
+                        console.print("[bold red]❌  Commit message not comfirmed. [/bold red]")
+                        
+                        exit()
                 
-            if options.comfirm:
-                c = Prompt.ask(
-                    prompt="Comfirm the message of the commit ? ",
-                    default="y", 
-                    show_default=True,
-                    show_choices=True,
-                    choices=["y", "n"],
-                )
+                if options.fact:
+                    commitly.unstage(".")
+                    commitly.add(", ".join(files))
+                    
+                commitly.commit()
+                console.print("[green]✔️  Commit message committed.[/green]")
                 
-                if c.lower() != "y":
-                    if options.del_temp:
-                        commitly.file_temp.unlink()
-                    
-                    if add != '!': commitly.unstage(add)
-                    
-                    console.print("[bold red]❌  Commit message not comfirmed. [/bold red]")
-                    
-                    exit()
-                    
-            
-            commitly.commit()
-            console.print("[green]✔️  Commit message committed.[/green]")
-            
-            
-            if options.push: 
-                commitly.push()
-                console.print("[green]✔️  Commit message pushed.[/green]")
+                
+                if options.push: 
+                    commitly.push()
+                    console.print("[green]✔️  Commit message pushed.[/green]")
         else:
             console.print("[bold red]❌  Error generating commit message. [/bold red]")
         
