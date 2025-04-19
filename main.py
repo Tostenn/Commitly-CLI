1791313a09108586bc622525d216e5501e3d742e
Tostenn <kouyatosten@gmail.com>
2025-04-19 13:03:33 +0200

    fix[main]: ajouter le nettoyage de l'écran avant l'exécution
    - Importation de `platform` et `system` pour gérer la commande de nettoyage en fonction du système d'exploitation.
    - Effacement de l'écran (cl `cls` pour Windows, `clear` pour Linux/macOS) avant de lancer la méthode run de CommitlyCLI.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 from cli import CommitlyCLI
+from sys import platform
+from os import system
 
 if __name__ == "__main__":
+    system('cls' if platform == 'win32' else 'clear')
     CommitlyCLI().run()
